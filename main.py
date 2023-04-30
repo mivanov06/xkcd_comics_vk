688eab662e768eabef887e5bc1bc27bb0648d0d7
mivanov06 <mivanov06@yandex.ru>
2023-04-30 18:41:52 +1000

    first commit

--- main.py
+++ main.py
@@ -1,61 +1,180 @@
 import os
-from random import randrange
-
+import random
 import requests
+
+from pathlib import Path
 from dotenv import load_dotenv
-from pprint import pprint
+from urllib.parse import urlparse
+from urllib.parse import unquote
 
-from main_function import download_image
+
+def fetch_random_comic() -> list():
+    '''
+    Получить ссылку и инфо на случайный комикс
+    :return:
+    image_url, image_alt
+    '''
+    url = "https://xkcd.com/"
+    info_path = "/info.0.json"
+    response = requests.get(f"{url}{info_path}")
+    response.raise_for_status()
+    count_comics = response.json()["num"]
+    comic_id = random.randint(1, count_comics)
+    response = requests.get(f"{url}{comic_id}{info_path}")
+    response.raise_for_status()
+    comic_meta = response.json()
+    image_url = comic_meta["img"]
+    image_alt = comic_meta["alt"]
+    return image_url, image_alt
 
 
-def get_xcds_count():
-    url = 'https://xkcd.com/info.0.json'
+def compose_filepath(url: str) -> Path:
+    url_part = urlparse(url).path
+    unquoted_url_part = unquote(url_part)
+    name = os.path.split(unquoted_url_part)[-1]
+    return Path(name)
+
+
+def save_image(url, path):
+    '''
+    Сохранить изображение по ссылке url в path
+    :param url:
+    :param path:
+    :return:
+    '''
     response = requests.get(url)
     response.raise_for_status()
-    comic_data = response.json()
-    return comic_data['num']
+    with open(path, "wb") as file:
+        file.write(response.content)
 
 
-def get_xcds_comics(number):
-    url = f'https://xkcd.com/{number}/info.0.json'
-    response = requests.get(url)
+def check_vk_error(response) -> None | str:
+    '''
+    Отображение ошибки vk
+    :param response:
+    :return:
+    '''
+    error = response.json().get("error")
+    if error:
+        code = error.get("error_code")
+        msg = error["error_msg"]
+        return f"[VK Error: {code}] {msg}" if code else msg
+    return
+
+
+def get_upload_url(api_url, token, api_version):
+    method = "photos.getWallUploadServer"
+    method_url = f"{api_url}method/{method}"
+    params = {
+        "access_token": token,
+        "v": api_version,
+    }
+    response = requests.get(method_url, params=params)
     response.raise_for_status()
+    error = check_vk_error(response)
+    if error:
+        raise requests.HTTPError(error)
+    return response.json()["response"]["upload_url"]
+
+
+def upload_to_server(upload_url, image):
+    method = "photos.saveWallPhoto"
+    method_url = f"{upload_url}{method}"
+    with open(image, "rb") as file:
+        files = {
+            "photo": file,
+        }
+        response = requests.post(method_url, files=files)
+    response.raise_for_status()
+    error = check_vk_error(response)
+    if error:
+        raise requests.HTTPError(error)
     return response.json()
 
 
-def get_wall_upload_server(vk_token, vk_group):
+def save_to_album(api_url, token, api_version, img_server, img_photo, img_hash):
+    method = "photos.saveWallPhoto"
+    method_url = f"{api_url}method/{method}"
     params = {
-        'group_id': 219613989,
-        'access_token': vk_token,
-        'v': '5.81'
+        "access_token": token,
+        "v": api_version,
+        "server": img_server,
+        "photo": img_photo,
+        "hash": img_hash,
     }
-    url = f'https://api.vk.com/method/photos.getWallUploadServer'
-    response = requests.get(url, params)
+    response = requests.post(method_url, params=params)
     response.raise_for_status()
-    return response.json()['response']['upload_url']
+    error = check_vk_error(response)
+    if error:
+        raise requests.HTTPError(error)
+    return response.json()
+
+
+def post_to_the_wall(api_url, token, api_version, owner_id, media_id, group_id, image_alt):
+    method = "wall.post"
+    method_url = f"{api_url}method/{method}"
+    params = {
+        "access_token": token,
+        "v": api_version,
+        "owner_id": f"-{group_id}",
+        "message": image_alt,
+        "from_group": 1,
+        "attachments": f"photo{owner_id}_{media_id}",
+    }
+    response = requests.get(method_url, params=params)
+    response.raise_for_status()
+    error = check_vk_error(response)
+    if error:
+        raise requests.HTTPError(error)
+
+
+def publish_to_vk(group_id, token, image, image_alt):
+    api_url = "https://api.vk.com/"
+    api_version = 5.131
+    upload_url = get_upload_url(
+        api_url,
+        token,
+        api_version,
+    )
+    upload_response = upload_to_server(upload_url, image)
+    img_server = upload_response["server"]
+    img_photo = upload_response["photo"]
+    img_hash = upload_response["hash"]
+    save_response = save_to_album(
+        api_url,
+        token,
+        api_version,
+        img_server,
+        img_photo,
+        img_hash,
+    )
+    owner_id = save_response["response"][0]["owner_id"]
+    media_id = save_response["response"][0]["id"]
+    post_to_the_wall(
+        api_url,
+        token,
+        api_version,
+        owner_id,
+        media_id,
+        group_id,
+        image_alt,
+    )
 
 
 def main():
     load_dotenv()
-    vk_client_id = os.getenv('VK_CLIENT_ID')
-    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
-
-    comics_number = randrange(get_xcds_count())
-    img_url = get_xcds_comics(comics_number)['img']
-    download_image(img_url, 'Images', prefix_name=comics_number)
-    # print(get_xcds_comics(comics_number)['alt'])
-
-     # url = 'https://api.vk.com/method/groups.get'
-    # params = {
-    #     'access_token': vk_access_token,
-    #     'v': '5.81'
-    # }
-    # response = requests.get(url, params=params)
-    # response.raise_for_status()
-    # print(response.json())
-
-    print(get_wall_upload_server(vk_access_token, vk_client_id))
+    group_id = os.getenv("VK_GROUP_ID")
+    token = os.getenv("VK_ACCESS_TOKEN")
+    image_url, image_alt = fetch_random_comic()
+    image_path = compose_filepath(image_url)
+    try:
+        save_image(image_url, image_path)
+        publish_to_vk(group_id, token, image_path, image_alt)
+    except requests.HTTPError:
+        raise
+    finally:
+        os.remove(image_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
